44885c747f34e177309d4dcb5626d27f0fa984d8
Deep <deep@Deeps-MacBook-Air.local>
2026-07-23 11:38:01 +0530

    chore: update 3 files

--- perameterised.java
+++ perameterised.java
@@ -11,10 +11,24 @@ class Student{
         System.out.println(this.name);
         System.out.println(this.age);
     }
+
+
+
+
+
+        Student (Student s2){
+        this.name = s2.name;
+        this.age = s2.age;
+    }
+    Student(){
+
+    }
 }
 public class perameterised {
     public static void main(String[] args){
-        Student s1 = new Student("Deep" , 20);
+        Student s1 = new Student("Deep" , 20);  //---------------perameterised------------------
+
+        Student s2 = new Student(s1);
     
         s1.print();
 
