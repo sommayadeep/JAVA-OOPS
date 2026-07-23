461ebf11de7bea6f37a2cfacc71bae701d1222aa
Deep <deep@Deeps-MacBook-Air.local>
2026-07-23 11:15:59 +0530

    chore: add 4, update 1 files

---
+++ nperameterised.java
@@ -0,0 +1,18 @@
+class Student{
+    String name;
+    int age;
+
+    public void print(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+public class nperameterised {
+    public void Student(){
+        Student s1 = new Student();
+        s1.name = "Deep";
+        s1.age = 20;
+
+
+    }
+}
