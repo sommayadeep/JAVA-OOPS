8de2cbf30e9f075ebb6919a33f7a745f1c89b365
Deep <deep@Deeps-MacBook-Air.local>
2026-07-23 11:31:39 +0530

    chore: update 3 files

--- nperameterised.java
+++ nperameterised.java
@@ -1,7 +1,12 @@
 class Student{
     String name;
     int age;
+    
+    Student( String name, int age){
+        this.name = name;
+        this.age = age;
 
+    }
     public void print(){
         System.out.println(this.name);
         System.out.println(this.age);
@@ -9,9 +14,7 @@ class Student{
 }
 public class nperameterised {
     public static void main(String[] args){
-        Student s1 = new Student();
-        s1.name = "Deep";
-        s1.age = 20;
+        Student s1 = new Student("Deep" , 20);
     
         s1.print();
 
