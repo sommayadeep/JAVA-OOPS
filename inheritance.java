34a0cdae7138db7d9b0b05487670e7b62ee56318
Deep <deep@Deeps-MacBook-Air.local>
2026-08-20 09:51:23 +0530

    refactor: update inheritance files and add new class binaries

--- inheritance.java
+++ inheritance.java
@@ -1,17 +1,32 @@
-class animal{
-    void eat(){
-        System.out.println("Eating");
+class A{
+    int i;
+    int j;
+    A(){
+        i=0;
+        j=0;
+    }
+    A(int i, int j){
+        this.i=i;
+        this.j=j;
+    }
+    void show(){
+        System.out.println("i="+i);
+        System.out.println("j="+j);
     }
 }
-class dog extends animal{
-    void bark(){
-        System.out.println("Barking");
+class B extends A{
+    int k;
+    B(int i, int j, int k){
+        super(i,j);
+        this.k=k;
+    }
+    void showk(){
+        System.out.println("k="+k);
     }
 }
-public class inheritance {
-    public static void main(String[] args){
-        dog d = new dog();
-        d.eat();
-        d.bark();
+public class inheritance{
+    public static void main (String[] args){
+        B b1 = new B(30,40,50);
+        b1.showk();
     }
 }
