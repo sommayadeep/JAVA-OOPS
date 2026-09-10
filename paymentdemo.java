e6bebf1aa34985e38444f089a2f915be592e557e
Deep <deep@Deeps-MacBook-Air.local>
2026-09-10 10:04:42 +0530

    refactor(payment): optimize payment classes and update demo implement...

--- paymentdemo.java
+++ paymentdemo.java
@@ -3,26 +3,31 @@ class Payment{
         System.out.println("payment of rs:" + amount);
     }
 }
-class CreditCardpayment{
-    void pay(double amount){
+class CreditCardpayment extends Payment {
+    @Override
+    void pay(double amount) {
         System.out.println("payment method: Credit Card");
         System.out.println("payment of rs:" + amount);
     }
 }
-class UPIpayment{
-    void pay(double amount){
+
+class UPIpayment extends Payment {
+    @Override
+    void pay(double amount) {
         System.out.println("payment method: UPI");
         System.out.println("payment of rs:" + amount);
     }
 }
+
 public class paymentdemo {
     public static void main (String[] args){
-    Payment payment;
-    payemnt = new CreditCardpayment();
-    payemnt.pay(500);
+        Payment payment;
 
-    payemnt = new UPIpayment();
-    payemnt.pay(500);
+        payment = new CreditCardpayment();
+        payment.pay(500);
+
+        payment = new UPIpayment();
+        payment.pay(500);
 
     }
 
